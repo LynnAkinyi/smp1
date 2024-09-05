bc28cb2909fbe13befbe9588ebdd8670ccb8f0fd
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-09-05 11:04:47 +0300

    router and page remounting

--- smpl1/src/App.js
+++ smpl1/src/App.js
@@ -1,28 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-function App() {
+export default function App() {
     return ( <
-        div className = "App" >
+        Router >
         <
-        header className = "App-header" >
+        div >
         <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
+        nav >
         <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
+        ul >
+        <
+        li > < Link to = "/" > Home < /Link></li >
+        <
+        li > < Link to = "/about" > About < /Link></li >
+        <
+        li > < Link to = "/contact" > Contact < /Link></li >
+        <
+        /ul> < /
+        nav >
+
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home key = "home" / > }
+        /> <
+        Route path = "/about"
+        element = { < About key = "about" / > }
+        /> <
+        Route path = "/contact"
+        element = { < Contact key = "contact" / > }
+        /> < /
+        Routes > <
+        /div> < /
+        Router >
     );
 }
-
-export default App;
---
+++ smpl1/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return ( <
+        div >
+        <
+        h1 className = "text-3xl font-bold underline" > Contact Page < /h1> <
+        p > Feel free to reach out to us. < /p> < /
+        div >
+    );
+};
+
+export default Contact;
